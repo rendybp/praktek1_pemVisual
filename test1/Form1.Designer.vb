51d2820d16349153b2425ceeeeca2f563e3c528f
rendybp <rendibuanap@gmail.com>
2023-03-30 18:08:21 +0700

    Membuat Tugas Pertama

---
+++ test1/Form1.Designer.vb
@@ -0,0 +1,355 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.namaInput = New System.Windows.Forms.TextBox()
+        Me.noDaftarInput = New System.Windows.Forms.TextBox()
+        Me.alamatInput = New System.Windows.Forms.TextBox()
+        Me.jenkelInput = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.jenkelOutput = New System.Windows.Forms.Label()
+        Me.alamatOutput = New System.Windows.Forms.Label()
+        Me.noDaftarOutput = New System.Windows.Forms.Label()
+        Me.namaOutput = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(214, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(137, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Form Data Pasien"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(47, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nama"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(47, 146)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(121, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "No Pendaftaran"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(47, 197)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Alamat"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(47, 244)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(106, 20)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Jenis Kelamin"
+        '
+        'namaInput
+        '
+        Me.namaInput.Location = New System.Drawing.Point(253, 101)
+        Me.namaInput.Name = "namaInput"
+        Me.namaInput.Size = New System.Drawing.Size(357, 26)
+        Me.namaInput.TabIndex = 5
+        '
+        'noDaftarInput
+        '
+        Me.noDaftarInput.Location = New System.Drawing.Point(253, 146)
+        Me.noDaftarInput.Name = "noDaftarInput"
+        Me.noDaftarInput.Size = New System.Drawing.Size(357, 26)
+        Me.noDaftarInput.TabIndex = 6
+        '
+        'alamatInput
+        '
+        Me.alamatInput.Location = New System.Drawing.Point(253, 197)
+        Me.alamatInput.Name = "alamatInput"
+        Me.alamatInput.Size = New System.Drawing.Size(357, 26)
+        Me.alamatInput.TabIndex = 7
+        '
+        'jenkelInput
+        '
+        Me.jenkelInput.Location = New System.Drawing.Point(253, 244)
+        Me.jenkelInput.Name = "jenkelInput"
+        Me.jenkelInput.Size = New System.Drawing.Size(357, 26)
+        Me.jenkelInput.TabIndex = 8
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.jenkelOutput)
+        Me.GroupBox1.Controls.Add(Me.alamatOutput)
+        Me.GroupBox1.Controls.Add(Me.noDaftarOutput)
+        Me.GroupBox1.Controls.Add(Me.namaOutput)
+        Me.GroupBox1.Location = New System.Drawing.Point(51, 321)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(559, 154)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Kesimpulan"
+        '
+        'jenkelOutput
+        '
+        Me.jenkelOutput.AutoSize = True
+        Me.jenkelOutput.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.jenkelOutput.Location = New System.Drawing.Point(320, 104)
+        Me.jenkelOutput.Name = "jenkelOutput"
+        Me.jenkelOutput.Size = New System.Drawing.Size(17, 20)
+        Me.jenkelOutput.TabIndex = 13
+        Me.jenkelOutput.Text = "  "
+        '
+        'alamatOutput
+        '
+        Me.alamatOutput.AutoSize = True
+        Me.alamatOutput.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.alamatOutput.Location = New System.Drawing.Point(320, 31)
+        Me.alamatOutput.Name = "alamatOutput"
+        Me.alamatOutput.Size = New System.Drawing.Size(17, 20)
+        Me.alamatOutput.TabIndex = 12
+        Me.alamatOutput.Text = "  "
+        '
+        'noDaftarOutput
+        '
+        Me.noDaftarOutput.AutoSize = True
+        Me.noDaftarOutput.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.noDaftarOutput.Location = New System.Drawing.Point(24, 104)
+        Me.noDaftarOutput.Name = "noDaftarOutput"
+        Me.noDaftarOutput.Size = New System.Drawing.Size(17, 20)
+        Me.noDaftarOutput.TabIndex = 11
+        Me.noDaftarOutput.Text = "  "
+        '
+        'namaOutput
+        '
+        Me.namaOutput.AutoSize = True
+        Me.namaOutput.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.namaOutput.Location = New System.Drawing.Point(24, 31)
+        Me.namaOutput.Name = "namaOutput"
+        Me.namaOutput.Size = New System.Drawing.Size(17, 20)
+        Me.namaOutput.TabIndex = 10
+        Me.namaOutput.Text = "  "
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(642, 103)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 36)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(642, 162)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 36)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.RadioButton4)
+        Me.GroupBox2.Controls.Add(Me.RadioButton3)
+        Me.GroupBox2.Controls.Add(Me.RadioButton2)
+        Me.GroupBox2.Controls.Add(Me.RadioButton1)
+        Me.GroupBox2.Location = New System.Drawing.Point(642, 244)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(192, 274)
+        Me.GroupBox2.TabIndex = 14
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Warna"
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(19, 196)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(70, 24)
+        Me.RadioButton4.TabIndex = 3
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Hijau"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(19, 146)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(83, 24)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Kuning"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(19, 91)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(62, 24)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Biru"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(19, 40)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(79, 24)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Merah"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.CheckBox3)
+        Me.GroupBox3.Controls.Add(Me.CheckBox2)
+        Me.GroupBox3.Controls.Add(Me.CheckBox1)
+        Me.GroupBox3.Location = New System.Drawing.Point(883, 244)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(192, 274)
+        Me.GroupBox3.TabIndex = 15
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Style"
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(16, 147)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(126, 24)
+        Me.CheckBox3.TabIndex = 2
+        Me.CheckBox3.Text = "Garis Bawah"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(16, 91)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(77, 24)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "Miring"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(16, 40)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(74, 24)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Tebal"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1171, 555)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.jenkelInput)
+        Me.Controls.Add(Me.alamatInput)
+        Me.Controls.Add(Me.noDaftarInput)
+        Me.Controls.Add(Me.namaInput)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents namaInput As System.Windows.Forms.TextBox
+    Friend WithEvents noDaftarInput As System.Windows.Forms.TextBox
+    Friend WithEvents alamatInput As System.Windows.Forms.TextBox
+    Friend WithEvents jenkelInput As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents jenkelOutput As System.Windows.Forms.Label
+    Friend WithEvents alamatOutput As System.Windows.Forms.Label
+    Friend WithEvents noDaftarOutput As System.Windows.Forms.Label
+    Friend WithEvents namaOutput As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+
+End Class
---
+++ test1/Form1.vb
@@ -0,0 +1,65 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        namaOutput.Text = namaInput.Text
+        noDaftarOutput.Text = noDaftarInput.Text
+        alamatOutput.Text = alamatInput.Text
+        jenkelOutput.Text = jenkelInput.Text
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton1.CheckedChanged
+        namaOutput.ForeColor = Color.Red
+        noDaftarOutput.ForeColor = Color.Red
+        alamatOutput.ForeColor = Color.Red
+        jenkelOutput.ForeColor = Color.Red
+        BackColor = Color.Aqua
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton2.CheckedChanged
+        namaOutput.ForeColor = Color.Blue
+        noDaftarOutput.ForeColor = Color.Blue
+        alamatOutput.ForeColor = Color.Blue
+        jenkelOutput.ForeColor = Color.Blue
+        BackColor = Color.LightPink
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton3.CheckedChanged
+        namaOutput.ForeColor = Color.Yellow
+        noDaftarOutput.ForeColor = Color.Yellow
+        alamatOutput.ForeColor = Color.Yellow
+        jenkelOutput.ForeColor = Color.Yellow
+        BackColor = Color.DarkGreen
+    End Sub
+
+    Private Sub RadioButton4_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton4.CheckedChanged
+        namaOutput.ForeColor = Color.Green
+        noDaftarOutput.ForeColor = Color.Green
+        alamatOutput.ForeColor = Color.Green
+        jenkelOutput.ForeColor = Color.Green
+        BackColor = Color.DarkOrange
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        namaOutput.Font = New Font("microsoft sans serif", namaOutput.Font.Size, namaOutput.Font.Style Xor FontStyle.Bold)
+        noDaftarOutput.Font = New Font("microsoft sans serif", noDaftarOutput.Font.Size, noDaftarOutput.Font.Style Xor FontStyle.Bold)
+        alamatOutput.Font = New Font("microsoft sans serif", alamatOutput.Font.Size, alamatOutput.Font.Style Xor FontStyle.Bold)
+        jenkelOutput.Font = New Font("microsoft sans serif", jenkelOutput.Font.Size, jenkelOutput.Font.Style Xor FontStyle.Bold)
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox2.CheckedChanged
+        namaOutput.Font = New Font("microsoft sans serif", namaOutput.Font.Size, namaOutput.Font.Style Xor FontStyle.Italic)
+        noDaftarOutput.Font = New Font("microsoft sans serif", noDaftarOutput.Font.Size, noDaftarOutput.Font.Style Xor FontStyle.Italic)
+        alamatOutput.Font = New Font("microsoft sans serif", alamatOutput.Font.Size, alamatOutput.Font.Style Xor FontStyle.Italic)
+        jenkelOutput.Font = New Font("microsoft sans serif", jenkelOutput.Font.Size, jenkelOutput.Font.Style Xor FontStyle.Italic)
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox3.CheckedChanged
+        namaOutput.Font = New Font("microsoft sans serif", namaOutput.Font.Size, namaOutput.Font.Style Xor FontStyle.Underline)
+        noDaftarOutput.Font = New Font("microsoft sans serif", noDaftarOutput.Font.Size, noDaftarOutput.Font.Style Xor FontStyle.Underline)
+        alamatOutput.Font = New Font("microsoft sans serif", alamatOutput.Font.Size, alamatOutput.Font.Style Xor FontStyle.Underline)
+        jenkelOutput.Font = New Font("microsoft sans serif", jenkelOutput.Font.Size, jenkelOutput.Font.Style Xor FontStyle.Underline)
+    End Sub
+End Class
